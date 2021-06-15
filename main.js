552616d450344939fa4093314e0c5c247eb95ade
katyadolgikh <78844412+katyadolgikh@users.noreply.github.com>
2021-06-15 21:59:35 +0300

    change name.html

--- main.js
+++
@@ -1,9 +0,0 @@
-// Select anchor document
-let more = document.querySelector('.more');
-// Return Array
-let audioArr = document.getElementsByTagName('audio');
-console.log(audioArr);
-// Add Event Listeners
-more.addEventListener('mouseenter', ()=>{
-    audioArr[0].play()
-});
